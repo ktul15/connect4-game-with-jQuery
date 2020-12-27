53e923695a486134a1326db0e3bf8063fa7d60fc
Ketul Makwana <ketulmakwana09@gmail.com>
2020-12-27 16:09:06 +0530

    add event listeners for hovering over cells

--- connect4.js
+++ connect4.js
@@ -4,6 +4,7 @@ class Connect4 {
     this.COLS = 7
     this.selector = selector
     this.createGrid()
+    this.setupEventListeners()
   }
 
   createGrid() {
@@ -12,13 +13,40 @@ class Connect4 {
       let $row = $('<div>')
         .addClass('row')
 
-      for (let cols = 0; cols < this.COLS; cols++) {
+      for (let col = 0; col < this.COLS; col++) {
         let $col = $('<div>')
           .addClass('col empty')
+          .attr('data-col', col)
+          .attr('data-row', row)
         $row.append($col)
       }
 
       $board.append($row)
+    }
+  }
+
+  setupEventListeners() {
+    const $board = $(this.selector)
+
+    $board.on('mouseenter', '.col.empty', function () {
+      const $col = $(this).data('col')
+      const $lastEmptyCell = findLastEmptyCell($col)
+      $lastEmptyCell.addClass('red')
+    })
+
+    $board.on('mouseleave', '.col.empty', function () {
+      $('.col').removeClass('red')
+    })
+
+    function findLastEmptyCell(col) {
+      const colEl = $(`.col[data-col=${col}]`)
+      for (let i = colEl.length - 1; i >= 0; i--) {
+        const $cell = $(colEl[i])
+        if ($cell.hasClass('empty')) {
+          return $cell
+        }
+      }
+      return null
     }
   }
 }
